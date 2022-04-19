08f4ad365ea1e3372f066a8820e4f30a118fe450
slavikbez <slavik@redocly.com>
2022-04-19 17:36:31 +0300

    revert accidentaly commited code

--- src/Markdown/MarkdownLayout.tsx
+++ src/Markdown/MarkdownLayout.tsx
@@ -1,15 +1,11 @@
 import React from '@portal/react';
 import { PageWrapper } from '@theme/Markdown/PageWrapper';
 import { ContentWrapper } from '@theme/Markdown/ContentWrapper';
-import { PageNavigation } from '@theme/PageNavigation/PageNavigation';
 
 export function MarkdownLayout({ tableOfContent, markdownWrapper }) {
   return (
     <PageWrapper data-component-name="Markdown/MarkdownLayout">
-      <ContentWrapper withToc={true}>
-        {markdownWrapper}
-        <PageNavigation />
-      </ContentWrapper>
+      <ContentWrapper withToc={true}>{markdownWrapper}</ContentWrapper>
       {tableOfContent}
     </PageWrapper>
   );
