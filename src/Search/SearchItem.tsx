969c3dc43266c7ad4fa5e1ea00949e5a1c5526f9
Oleksii <oleksii.horbachevskyi@redocly.com>
2022-04-21 16:03:15 +0300

    chore: upgrade search item layout and styles due to new requirements

--- src/Search/SearchItem.tsx
+++ src/Search/SearchItem.tsx
@@ -9,33 +9,66 @@ interface SearchItemProps {
   item: ActiveItem<SearchDocument>;
 }
 
+const MAX_SEARCH_ITEMS = 2;
+
 export function SearchItem({ item }: SearchItemProps) {
   const ref = useRef<HTMLAnchorElement>();
 
-  const highlight = ([pre, entry, suf]: string[]) => (
-    <>
+  const highlight = (text: string | string[]) => {
+    if (!Array.isArray(text)) return text;
+
+    const [pre, entry, suf] = text;
+    return <>
       {pre}
-      <strong>{entry}</strong>
+        <Strong>{entry}</Strong>
       {suf}
     </>
-  );
+  };
+
+  let moreItems = 0;
+
+  if (item.parameters.length > MAX_SEARCH_ITEMS) {
+    moreItems = item.parameters.length - MAX_SEARCH_ITEMS;
+    item.parameters.length = MAX_SEARCH_ITEMS;
+  }
 
   useEffect(() => {
     if (item.active) {
       ref.current.focus();
     }
   }, [item.active]);
+
   return (
     <SearchLink to={item.url} tabIndex={0} innerRef={ref}>
+      <Operaition>
+        {item.httpVerb ? <OperationBadge type={item.httpVerb}>{item.httpVerb}</OperationBadge> : null}
+        {highlight(item.pathName)}
+      </Operaition>
       <Title>
-        {item.httpVerb && <Badge type={item.httpVerb}>{item.httpVerb}</Badge>}
-        {item.title}
+        {highlight(item.title)}
       </Title>
-      <Description>{highlight(item.chunks)}</Description>
-      <Path>
-        {item.place && (item.place + ' > ')}
-        {item.path.join(' > ')}
-      </Path>
+      <Description>
+        {highlight(item.text)}
+      </Description>
+
+      {item.parameters?.length
+        ?
+        <Parameters>
+          {item.parameters.map(param => (
+            <>
+              <Place>
+                {param.place} → {highlight(param.name)} <br/>
+                {highlight(param.description)}
+              </Place>
+            </>
+          ))}
+          {moreItems ? <MoreText>and {moreItems} more...</MoreText> : null}
+        </Parameters>
+        :
+        <Path>
+          {item.path?.join(' → ')}
+        </Path>
+      }
     </SearchLink>
   );
 }
@@ -55,7 +88,33 @@ const SearchLink = styled(Link)`
   }
 `;
 
+const Strong = styled.strong`
+  color: var(--color-primary-main);
+`
+
+const Parameters = styled.div`
+  border-left: 1px solid var(--color-border-light);
+  padding: 0 12px;
+  margin-top: 8px;
+  margin-bottom: 4px;
+`
+
+const MoreText = styled.div`
+  font-size: 12px;
+  font-weight: 100;
+  padding-top: 4px;
+`
+
+const Operaition = styled.div`
+  font-weight: 200;
+  font-size: 12px;
+  color: var(--search-item-title-text-color);
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
 const Title = styled.div`
+  line-height: 22px;
   font-weight: 600;
   color: var(--search-item-title-text-color);
   overflow: hidden;
@@ -64,19 +123,22 @@ const Title = styled.div`
 
 const Description = styled.div`
   font-size: 14px;
-  line-height: 22px;
   overflow: hidden;
   text-overflow: ellipsis;
+`;
+
+const Place = styled.div`
+  font-size: 12px;
+  padding-top: 8px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  
+  &:first-child {
+    padding-top: 0;
+  }
 `;
 
 const Path = styled.div`
   font-size: 12px;
   line-height: 22px;
-  overflow: hidden;
-  text-overflow: ellipsis;
-`;
-
-const Badge = styled(OperationBadge)`
-  position: relative;
-  bottom: 2px;
 `;
