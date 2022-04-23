554916af8b2b5b2846647cc29d9fa92c73b9c989
Roman Marshevskyy <roman.marshevskyy@redoc.ly>
2022-04-23 11:27:06 +0300

    chore: progress on ci flow

---
+++ src/Footer/__tests__/Footer.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+describe('#Footer', () => {
+  it('should render Footer', () => {
+    expect(true).toBe(true);
+  });
+});
