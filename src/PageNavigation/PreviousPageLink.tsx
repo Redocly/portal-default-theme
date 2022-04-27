8711ebf12a48c17270d3c7355feb40e26f5ca3f1
slavikbez <slavik@redocly.com>
2022-04-27 12:42:57 +0300

    usePageData is now available

--- src/PageNavigation/PreviousPageLink.tsx
+++ src/PageNavigation/PreviousPageLink.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import type { ResolvedNavLinkItem } from '@portal/types';
-// import { usePageData } from '@portal/hooks/usePageData';
+import { usePageData } from '@portal/hooks/usePageData';
 import { Button } from '@theme/ui';
 
 type PreviousPageLink = ResolvedNavLinkItem | null;
 
 export function PreviousPageLink() {
-  const prevPage = null;
-  // TODO: uncomment when usePageData is merged in new-hope
-  // const { prevPage }: PreviousPageLink = usePageData();
+  const { prevPage }: PreviousPageLink = usePageData();
   if (!prevPage) {
     return <div>&nbsp;</div>
   }
